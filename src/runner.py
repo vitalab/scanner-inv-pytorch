6324a947aa4d187673959b26080d28b7b39af609
lemairecarl <c.lemaire@live.ca>
2022-06-27 17:46:58 -0400

    Track metrics using comet

--- src/runner.py
+++ src/runner.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import comet_ml
 import torch
 #import loader
 import arch
@@ -36,13 +37,14 @@ LR=1e-4
 adv_LR=1e-4
 batch_size=128
 save_freq=5
+n_sh_coeff = 28
 
 scan_type_map = {
     "1200" : 0,
     "7T" : 1
 }
 
-dataset = TractoinfernoDataset(Path('/home/carl/data/tractoinferno/masked_full'), 'trainset', 28, force_preprocess=True)
+dataset = TractoinfernoDataset(Path('/home/carl/data/tractoinferno/masked_full'), 'trainset', n_sh_coeff)
 
 train_loader = torch.utils.data.DataLoader(
     dataset,
@@ -56,7 +58,7 @@ train_loader = torch.utils.data.DataLoader(
 center_vox_func = None
 
 #vec_size = 322
-vec_size = dataset.n_sh_coeff * 7
+vec_size = n_sh_coeff * 7
 
 enc_obj = arch.encoder( vec_size, 32 )
 dec_obj = arch.decoder( 32, vec_size, 1 )
@@ -73,26 +75,34 @@ optimizer = torch.optim.Adam(
 adv_optimizer = torch.optim.Adam(adv_obj.parameters(), lr=adv_LR)
 
 loss_weights = {
-    "recon" : 1.0,\
-    "prior" : 1.0,\
-    "projection" : 1.0,\
-    "marg" : 0.01,\
-    "adv" : 10.0\
+    "recon" : 1.0,
+    "prior" : 1.0,
+    "projection" : 1.0,
+    "marg" : 0.01,
+    "adv" : 10.0
 }
 
+comet_experiment = comet_ml.Experiment(project_name='harmon_moyer')
+comet_experiment.log_parameters({
+    'n_adv_per_enc': n_adv_per_enc,
+    'burnin_steps': burnin_steps,
+    'LR': LR,
+    'adv_LR': adv_LR,
+    'batch_size': batch_size,
+    'save_freq': save_freq,
+    'n_sh_coeff': n_sh_coeff
+})
+comet_experiment.log_parameters(loss_weights, prefix='loss_weight')
+
 print('Training starts')
+
+global_step = 0
 for epoch in range(n_epochs):
-
-    train_loss = 0
-    adv_loss = 0
-    n = 1
-    n_adv = 1
-
-    total_recon_loss = 0
-    total_kl_loss = 0
-    total_proj_loss = 0
-    total_marg_loss = 0
-    total_adv_loss = 0
+    recon_loss = torch.tensor(0)
+    kl_loss = torch.tensor(0)
+    proj_loss = torch.tensor(0)
+    marg_loss = torch.tensor(0)
+    gen_adv_loss = torch.tensor(0)
 
     for d_idx, batch in enumerate(tqdm(train_loader)):
         #print(f"batch {d_idx}", flush=True)
@@ -116,9 +126,6 @@ for epoch in range(n_epochs):
 
             loss.backward(retain_graph=True)
             adv_optimizer.step()
-            adv_loss += loss.item()*x.size()[0]
-            n_adv += x.size()[0]
-
         else:
 
             optimizer.zero_grad()
@@ -129,32 +136,22 @@ for epoch in range(n_epochs):
                 loss_weights, 32
             )
 
-            total_recon_loss += recon_loss.item()*x.size()[0]
-            total_kl_loss += kl_loss.item()*x.size()[0]
-            #total_proj_loss += proj_loss.item()*x.size()[0]
-            total_marg_loss += marg_loss.item()*x.size()[0]
-            total_adv_loss += gen_adv_loss.item()*x.size()[0]
-
             loss.backward(retain_graph=True)
             optimizer.step()
-            train_loss += loss.item()*x.size()[0]
-            n += x.size()[0]
+
+        global_step += 1
 
         #del x, x_subj_space, sh_mat, sh_weights, c
 
-        if d_idx % 200 == 0:
-            print("epoch",epoch)
-            print("train loss total",train_loss / n)
-            print("adv loss total",adv_loss / n_adv)
-            print(
-                "train loss, recon", total_recon_loss / n,
-                "kl", total_kl_loss/ n,
-                "proj", total_proj_loss/ n,
-                "marg", total_marg_loss/ n,
-                "adv", total_adv_loss/ n
-            )
+        comet_experiment.log_metrics({
+            'loss': loss.item(),
+            'loss_recon': recon_loss.item(),
+            'loss_kl': kl_loss.item(),
+            'loss_marg': marg_loss.item(),
+            'loss_adv': gen_adv_loss.item()
+        }, step=global_step)
 
-    if save_path is not None and epoch > burnin and epoch % save_freq == 0:
+    if save_path is not None and epoch % save_freq == 0:
         torch.save(
             {
                 "enc":enc_obj.state_dict(),
@@ -163,21 +160,3 @@ for epoch in range(n_epochs):
             },
             f"{save_path}/{epoch}.pth"
         )
-
-    if False:  # epoch != 0 and d_idx < burnin_steps:
-        print("epoch",epoch)
-        print("train loss total",train_loss / n)
-        print("adv loss total",adv_loss / n_adv)
-        print(
-            "train loss, recon", total_recon_loss / n,
-            "kl", total_kl_loss/ n,
-            "proj", total_proj_loss/ n,
-            "marg", total_marg_loss/ n,
-            "adv", total_adv_loss/ n
-        )
-
-
-
-
-
-
